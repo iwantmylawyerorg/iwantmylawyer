e3c3cbb8b2bd0870f6cb59496cc9805a9dab8a67
batikanyeni <batikanyeni@outlook.com>
2024-03-09 14:37:51 +0300

    swagger whitelisted

--- src/main/java/com/thesis/iwantmylawyer/security/SecurityConfig.java
+++ src/main/java/com/thesis/iwantmylawyer/security/SecurityConfig.java
@@ -18,6 +18,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private static final String[] WHITE_LIST_URL = {
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/api/v1/auth/**",
+            "/api/v1/lawyer/**",
+            "/api/v1/client/**"};
     private final JwtFilter jwtFilter;
     private final SecurityUserService securityUserService;
     private final PasswordEncoder passwordEncoder;
@@ -33,7 +47,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(x ->x
-                        .requestMatchers("/api/v1/auth/**","/api/v1/lawyer/**","/api/v1/client/**").permitAll()
+                        .requestMatchers(WHITE_LIST_URL).permitAll()
                 )
                 .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
--- src/main/java/com/thesis/iwantmylawyer/user/UserResponse.java
+++ src/main/java/com/thesis/iwantmylawyer/user/UserResponse.java
@@ -1,6 +1,7 @@
 package com.thesis.iwantmylawyer.user;
 
 public record UserResponse(
+        String id,
         String email,
         String firstName,
         String lastName
