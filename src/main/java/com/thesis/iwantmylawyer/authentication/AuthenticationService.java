05b49cdebc6a2b76022340005862a67a6f2ba519
batikanyeni <batikanyeni@outlook.com>
2024-05-21 15:55:32 +0300

    fix

--- src/main/java/com/thesis/iwantmylawyer/authentication/AuthenticationService.java
+++ src/main/java/com/thesis/iwantmylawyer/authentication/AuthenticationService.java
@@ -31,24 +31,6 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final UserConverter userConverter;
 
-  /*  public AuthenticationResponse register(RegisterRequest request) {
-        var user = User.builder()
-                .firstname(request.getFirstname())
-                .lastname(request.getLastname())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(request.getRole())
-                .build();
-        var savedUser = repository.save(user);
-        var jwtToken = jwtService.generateToken(user);
-        var refreshToken = jwtService.generateRefreshToken(user);
-        saveUserToken(savedUser, jwtToken);
-        return AuthenticationResponse.builder()
-                .accessToken(jwtToken)
-                .refreshToken(refreshToken)
-                .build();
-    } */
-
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                         request.email(), request.password()));
